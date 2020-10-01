f9b15f47c8ab26f684c966134efdec1f49356eed
pallavi002 <lodhi.pallavi18@siesgst.ac.in>
2020-10-01 12:05:45 +0530

    made code clean using middleware

---
+++ middleware.js
@@ -0,0 +1,6 @@
+app.use(express.static('public'));
+
+//make way for some custom css, js and images
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));

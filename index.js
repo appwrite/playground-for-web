f23a4629c8a91d39b2442b1af9931ccf055d810f
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-09-01 12:57:19 +0200

    feat(demo): add realtime

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-var express = require("express");
+const express = require("express");
 
-var app = express();
- 
+const app = express();
+
 app.use(express.static('public'));
- 
-//make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
- 
-var server = app.listen(8000, function(){
-    var port = server.address().port;
+
+const server = app.listen(8000, function () {
+    const port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });

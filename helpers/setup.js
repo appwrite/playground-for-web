b4a8aea926054d40aa664a7a6419863e8438924f
Alex Patterson <alex@codingcat.dev>
2022-09-28 18:04:19 -0400

    additional details on setup

--- helpers/setup.js
+++ helpers/setup.js
@@ -9,10 +9,10 @@ const storage = new sdk.Storage(client);
 client
   .setSelfSigned(true)
   .setProject("playground")
+  // Update this key
   .setKey(
     "beb1e9fdeece6048ffa707a8cf19f3f645bce784def5135f3f8f5c91e382570ac26df1180105ba50068df79a20687fbc5b2af525fe2962550f42840175656444584cc84e97ec4b5ae57e342b912949c98224bac9d64ba6f3531ba33da707a07e0b0adb4155b95fbd32918aafba87338ab4c131f76d786cff4406c2af6bba9ef4"
   )
-  // .setJWT('jwt') // set this to authenticate using JWT
   .setEndpoint("https://localhost/v1");
 
 (async function () {

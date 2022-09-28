bafdb4a5f8ea8aace012bd54fe3e32379a20379a
Alex Patterson <alex@codingcat.dev>
2022-09-28 17:58:04 -0400

    update to match playground

--- test.js
+++ test.js
@@ -1,19 +1,23 @@
-const sdk = require('node-appwrite');
+const sdk = require("node-appwrite");
 
 const client = new sdk.Client();
 
 client
-    .setEndpoint('http://localhost/v1') // Your API Endpoint
-    .setProject('62387717329626ff1d5a') // Your project ID
-    .setKey('000615ffba60ade50ebabc4f13360f4b78a87b40d6f64e2d3eb3d7b248ed70495f8e6918327c12338d96e1f5a745360db915b7ec71e4e0694cf3c3bc4f9a33a78a183d651f64e75ba1f2f3b920a7fec5b04cb706ddbe962d6922a9631e855198e9d042c8205efcc45a19640e0f55b543570da692fe5bea94381632f161b10913') // Your secret API key
-;
+  .setEndpoint("http://localhost/v1") // Your API Endpoint
+  .setProject("playground") // Your project ID
+  .setKey(
+    "beb1e9fdeece6048ffa707a8cf19f3f645bce784def5135f3f8f5c91e382570ac26df1180105ba50068df79a20687fbc5b2af525fe2962550f42840175656444584cc84e97ec4b5ae57e342b912949c98224bac9d64ba6f3531ba33da707a07e0b0adb4155b95fbd32918aafba87338ab4c131f76d786cff4406c2af6bba9ef4"
+  ); // Your secret API key
 
 let users = new sdk.Users(client);
 
-let promise = users.get('623b66a849afa34b5d4e');
+let promise = users.get("6334bfa0202f3a501716");
 
-promise.then(function (response) {
+promise.then(
+  function (response) {
     console.log(response);
-}, function (error) {
+  },
+  function (error) {
     console.log(error);
-});
+  }
+);
